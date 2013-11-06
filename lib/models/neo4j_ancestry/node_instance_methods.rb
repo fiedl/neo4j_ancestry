75a19055619dd16f89d8db447f0ae34fc08e69cf
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2013-11-06 18:44:29 +0100

    reverting back to cypher, because the type selection then can be done on the query level rather than filtering an array.

--- lib/models/neo4j_ancestry/node_instance_methods.rb
+++ lib/models/neo4j_ancestry/node_instance_methods.rb
@@ -31,30 +31,30 @@ module Neo4jAncestry
     # ancestors, descendants and siblings.
     #
     def parents
-      # match (self)<-[:is_parent_of]-(parents)
-      self.neo_node.incoming(:is_parent_of).to_active_record
+      find_related_nodes_via_cypher("
+        match (parents)-[:is_parent_of]->(self)
+        return parents
+      ")
     end
     def children
-      # match (self)-[:is_parent_of]->(children)
-      self.neo_node.outgoing(:is_parent_of).to_active_record
+      find_related_nodes_via_cypher("
+        match (self)-[:is_parent_of]->(children)
+        return children
+      ")
     end
     def ancestors(options = {})
-      # match (self)<-[:is_parent_of*1..100]->(ancestors)
-      self.neo_node.incoming(:is_parent_of).depth(100).to_active_record
+      find_related_nodes_via_cypher("
+        match (self)<-[:is_parent_of*1..100]-(ancestors)
+        #{where('ancestors.ar_type' => options[:type])}
+        return ancestors
+      ").uniq
     end
     def descendants(options = {})
-      # match (self)-[:is_parent_of*1..100]->(descendants)
-      objs = self.neo_node.outgoing(:is_parent_of).depth(100).to_active_record
-      objs.select! { |obj| obj.kind_of? options[:type].constantize } if options[:type].present?
-      return objs
-      
-      # where_clause_for_type = "where descendants.ar_type = '#{options[:type]}'" if options[:type]
-      # where_clause_for_type ||= ""
-      # find_related_nodes_via_cypher("
-      #   match (self)-[:is_parent_of*1..100]->(descendants)
-      #   #{where_clause_for_type}
-      #   return descendants
-      # ").uniq
+      find_related_nodes_via_cypher("
+        match (self)-[:is_parent_of*1..100]->(descendants)
+        #{where('descendants.ar_type' => options[:type])}
+        return descendants
+      ").uniq
     end
     def siblings
       find_related_nodes_via_cypher("
@@ -77,7 +77,7 @@ module Neo4jAncestry
   
       self.neo_node.all_paths_to(other_object.neo_node)
         .both(:is_parent_of).depth(100).nodes.collect do |path|
-          neo_nodes_to_objects(path)
+          path.collect { |neo_node| neo_node.to_active_record }
         end
       
     end
@@ -117,62 +117,30 @@ module Neo4jAncestry
     #
     def cypher_results_to_objects(cypher_results)
       cypher_results["data"].collect do |result|
-        #result.first["data"]["ar_type"].constantize.find(result.first["data"]["ar_id"])
-        neo_node_to_object(result.first["data"])
+        result.first["data"]["ar_type"].constantize.find(result.first["data"]["ar_id"])
       end
     end
     private :cypher_results_to_objects
     
-    # Neography often returns an encapsulated array of neo_nodes, e.g.
-    #    [[neo_node1, neo_node2 ...]]
+    # This is a helper for Cypher where clauses.
+    # Example:
     #
-    # This method returns an array of the associated ActiveRecord objects.
+    #     where( 'user.name' => 'Bob', 'user.age' => 16 )
     #
-    def neo_nodes_to_objects(neo_nodes)
-      neo_nodes_array = neo_nodes.first if neo_nodes.first.kind_of? Array
-      neo_nodes_array ||= neo_nodes
-      neo_nodes_array.collect do |neo_node|
-        neo_node_to_object neo_node
+    # results in
+    #
+    #     "WHERE user.name = 'Bob' AND 'user.age' = '16'"
+    #
+    def where(conditions_hash = {})
+      conditions_array = []
+      conditions_hash.each do |key, value|
+        conditions_array << "#{key} = '#{value}'" if value.present?
       end
+      conditions_str = ""
+      if conditions_array.count > 0
+        conditions_str = "WHERE " + conditions_array.join(" AND ")
+      end
+      return conditions_str
     end
-    private :neo_nodes_to_objects
-    
-    # This method finds the ActiveRecord object that corresponds to 
-    # a NeoNode. 
-    #
-    # TODO: I have not found a method on neo_node that simply returns the 
-    # ActiveRecord object. If you know this method, please change this
-    # method accordingly.
-    #
-    def neo_node_to_object(neo_node)
-      neo_node["ar_type"].constantize.find neo_node["ar_id"]
-    end
-    private :neo_node_to_object
-    
-    # # This method returns an ActiveRecord::Relation that refers to the
-    # # ActiveRecord object associated with the given neo_node.
-    # #
-    # # This might be useful if one would like to apply further where
-    # # clauses, et cetera.
-    # #
-    # def neo_node_to_arel(neo_node)
-    #   neo_node["ar_type"].where id: neo_node["ar_id"]
-    # end
-    # private :neo_node_to_arel
-    # 
-    # def neo_nodes_to_arel(neo_nodes)
-    #   # TODO: How can multiple ActiveRecord::Relations contained in an Array
-    #   # be combined to a single ActiveRecord::Relation, joined by the OR
-    #   # operator?
-    #   # 
-    #   # There are a couple of possibilities:
-    #   #   * http://stackoverflow.com/questions/9540801/combine-two-activerecordrelation-objects
-    #   #   * http://stackoverflow.com/questions/7976358/activerecord-arel-or-condition
-    #   #   * http://railscasts.com/episodes/355-hacking-with-arel
-    #   # 
-    #   # But, for the moment, I'm not sure if this is worth the efford.
-    # end
-    # private :neo_nodes_to_arel
-    
   end
 end
